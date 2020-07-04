d02a4d187e3cf04be9f7b7dbbe7f482f537e0045
silentstone <3944679@qq.com>
2020-07-04 22:36:17 +0800

    refine

--- N849.cpp
+++ N849.cpp
@@ -1,50 +1,39 @@
 class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
-        int mstart, mend; 
         int max_dist = -1;
          
-        bool searching_end = false;
+        bool searching_start = true;
         int start, end;
-        int distance; 
+        int distance;
         for (int i = 0; i < seats.size(); i ++) {
-            if (searching_end) {
-                if (seats[i] != 0) {
-                    searching_end = false;
-                    end = i - 1;
-                    int distance;
-                    if (start != 0)
-                       distance = (end - start) / 2 + 1;
-                    else 
-                       distance = end - start + 1;
-
-                    if (distance > max_dist) 
-                        max_dist = distance;
-                }
-                else if (i == seats.size() - 1) {
-                    searching_end = false;
-                    end = i;
-                    
-                    int distance = end - start + 1;
-                    if (distance > max_dist) {
-                        max_dist = distance;
-                    }
+            if (searching_start) {
+                if (seats[i] == 0) {
+                    start = i;
+                    searching_start = false;
                 }
             }
             else {
-                if (seats[i] == 0)  {
-                    start = i;
-                    searching_end = true;
+                if (seats[i] == 1) {
+                    end = i - 1;
+                    searching_start = true;
+                    
+                    if (start == 0) {
+                        distance = end - start + 1;
+                    }
+                    else {
+                        distance = (end - start) / 2 + 1;
+                    }
+                    max_dist = max(distance, max_dist);
                 }
-           }
+            }
         }
-
-        if (searching_end && start == seats.size() - 1 ) {
-            if (max_dist < 1) 
-               max_dist = 1;
+      
+        if (!searching_start) {
+            distance =  seats.size() - start;
+            max_dist = max(distance, max_dist);
         }
-
+   
         return max_dist;
-
     }
 };
