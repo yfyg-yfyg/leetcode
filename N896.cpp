af38e99bb94906c494ff79212f38f4d7624b5f99
silentstone <3944679@qq.com>
2020-07-17 20:57:46 +0800

    N896

---
+++ N896.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& A) {
+        bool ans = true;
+
+        int i = 0; 
+        for (; i < A.size() - 1; i ++) {
+            if (A[i] != A[i + 1]) 
+                break;
+        }
+
+        if (i == A.size() - 1)
+            return true;
+
+        bool asending = true;
+        if (A[i] > A[i+1]) 
+            asending = false; 
+
+        for ( i = i + 1; i < A.size() - 1; i ++) {
+            if (A[i] > A[i+1] && asending || A[i] < A[i+1] && !asending)
+                return false;
+        }
+   
+        return ans;
+    }
+};
