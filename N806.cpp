0b26af9a0eee669349de68ac93f8ee23af1ef732
silentstone <3944679@qq.com>
2020-07-23 23:12:56 +0800

    N806

--- N806.cpp
+++ N806.cpp
@@ -27,3 +27,26 @@ public:
     }
 };
 
+class Solution {
+public:
+    vector<int> numberOfLines(vector<int>& widths, string S) {
+
+        int sum = 0;
+        int line = 1;
+        for (int i = 0; i < S.length(); ) {
+            int width = widths[S[i] - 'a'];
+            if (sum + width > 100) {
+                line ++;
+                sum = 0;
+            }
+            else {
+                sum += width;
+                i ++;
+            }
+        }
+
+
+        return {line, sum};
+    }
+};
+
