4eeb76f77e38a315720a1e2c72b86399166c2f69
silentstone <3944679@qq.com>
2020-07-29 11:52:51 +0800

    N933

--- N933.cpp
+++ N933.cpp
@@ -19,3 +19,20 @@ public:
      }
 };
 
+
+class RecentCounter {
+    deque<int> timestamps;
+public:
+    RecentCounter() {
+    }
+
+    int ping(int t) {
+
+        while(!timestamps.empty() && t - timestamps.front() > 3000) {
+            timestamps.pop_front();
+        }
+
+        timestamps.push_back(t);
+        return timestamps.size();
+     }
+};
