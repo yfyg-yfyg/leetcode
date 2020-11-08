32b341e3c7e3ea9d384305ed75251e1064879056
silentstone <3944679@qq.com>
2020-11-08 10:49:45 -0600

    use array instead of Arraylist

--- N1137.java
+++ N1137.java
@@ -1,27 +1,30 @@
+
 class Solution {
     public int tribonacci(int n) {
+
         if (n == 2 || n == 1) {
             return 1;
         }
         else if (n == 0) {
             return 0;
-        } 
-        else {
-            ArrayList<Integer>  series = new ArrayList<>();
-            series.add(0);
-            series.add(1);
-            series.add(1);
-            
-            for (int i = 3; i <= n ; i ++) {
-                series.add(series.get(i-1) + series.get(i-2) + series.get(i-3));
-            }
-            
-            
-            return series.get(series.size() - 1);
-            
-            
         }
-       
-        
+        else {
+
+            int[] series = new int[3];
+            series[0] = 0;
+            series[1] = 1;
+            series[2] = 1;
+
+            for (int i = 3; i <= n ; i ++) {
+                series[i % 3] = series[0] + series[1] + series[2];
+            }
+
+
+            return series[n % 3];
+
+
+        }
+
+
     }
 }
