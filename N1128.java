945d0ee7e31c0bd5e32ed6c97ac4d5b7a194cabe
silentstone <3944679@qq.com>
2020-11-07 23:15:53 -0600

    use array instead of hashmap

--- N1128.java
+++ N1128.java
@@ -1,18 +1,17 @@
 class Solution {
-    
+   
     private int combine(int n) {
        return n *(n -1) / 2;
     }
     
     public int numEquivDominoPairs(int[][] dominoes) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
         int ans = 0;
+        int[]  map = new int[100];
+        
         for(int[] e : dominoes) {
-            int key = Math.min(e[0], e[1]) + Math.max(e[0], e[1]) * 10;
-            ans += map.merge(key, 0, (a , b) -> a + 1 );
+            ans += map[Math.min(e[0], e[1]) + Math.max(e[0], e[1]) * 10] ++;
         }
-      
+        
         return ans;
     }
 }
