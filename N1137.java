46119547f71b9a39e7a6aada6fb05e51c9567375
silentstone <3944679@qq.com>
2020-11-08 10:48:48 -0600

    use loop

--- N1137.java
+++ N1137.java
@@ -1,15 +1,27 @@
 class Solution {
     public int tribonacci(int n) {
-        if (n >= 3) {
-            return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
-        }
-        else if (n == 2 || n == 1) {
+        if (n == 2 || n == 1) {
             return 1;
         }
-        else {
+        else if (n == 0) {
             return 0;
+        } 
+        else {
+            ArrayList<Integer>  series = new ArrayList<>();
+            series.add(0);
+            series.add(1);
+            series.add(1);
+            
+            for (int i = 3; i <= n ; i ++) {
+                series.add(series.get(i-1) + series.get(i-2) + series.get(i-3));
+            }
+            
+            
+            return series.get(series.size() - 1);
+            
+            
         }
+       
         
     }
 }
-
