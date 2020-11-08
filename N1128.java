6d0ba3003729b6d9eb591f5bec11210283912705
silentstone <3944679@qq.com>
2020-11-07 23:14:32 -0600

    add up when counting

--- N1128.java
+++ N1128.java
@@ -1,26 +1,18 @@
 class Solution {
     
-    int ans = 0;
     private int combine(int n) {
        return n *(n -1) / 2;
     }
     
     public int numEquivDominoPairs(int[][] dominoes) {
-        
         HashMap<Integer, Integer> map = new HashMap<>();
         
+        int ans = 0;
         for(int[] e : dominoes) {
             int key = Math.min(e[0], e[1]) + Math.max(e[0], e[1]) * 10;
-            map.merge(key, 1, (a , b) -> a + 1 );
+            ans += map.merge(key, 0, (a , b) -> a + 1 );
         }
-        
-        map.forEach((k, v) -> {
-           if (v > 1) 
-               ans += combine(v);
-        });
       
-        
         return ans;
     }
 }
-
