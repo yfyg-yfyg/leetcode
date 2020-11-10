6c9910fca5cd9d8a3488e2d50e5f8f6d35339eec
silentstone <3944679@qq.com>
2020-11-10 13:14:30 -0600

    modify the way to calculate leap year

--- N1154.java
+++ N1154.java
@@ -1,7 +1,7 @@
 class Solution {
     public int dayOfYear(String date) {
-       int[][] daysInMonth = {
-                         {0,
+       int[] daysInMonth = {
+                          0,
                           31, 
                           31+28,
                           31+28+31,
@@ -14,56 +14,23 @@ class Solution {
                           31+28+31+30+31+30+31+31+30+31,
                           31+28+31+30+31+30+31+31+30+31+30,
                           31+28+31+30+31+30+31+31+30+31+30+31
-                          },
-                          {
-                          0,
-                          31, 
-                          31+29,
-                          31+29+31,
-                          31+29+31+30,
-                          31+29+31+30+31,
-                          31+29+31+30+31+30,
-                          31+29+31+30+31+30+31,
-                          31+29+31+30+31+30+31+31,
-                          31+29+31+30+31+30+31+31+30,
-                          31+29+31+30+31+30+31+31+30+31,
-                          31+29+31+30+31+30+31+31+30+31+30,
-                          31+29+31+30+31+30+31+31+30+31+30+31}
-                         };
-        
+                          };
+                          
         String[] dateString = date.split("-"); 
         Integer[] dateInteger = new Integer[dateString.length]; 
         for (int i = 0; i < dateString.length; i ++) {
            dateInteger[i] = Integer.parseInt(dateString[i]); 
         }
         
-        //Arrays.stream(date.split("-")).map(Integer::parseInt).collect(toList());
-  
-        int ans = 0;
-        Boolean runYear = false;
-        if (dateInteger[0] % 100 == 0) {
-            if (dateInteger[0] % 400 == 0) {
-                runYear = true;
-            }
-            else {
-                runYear = false;
-            }
-        }
-        else if (dateInteger[0] % 4 == 0) {
-            runYear = true;
+        if (dateInteger[0] % 4 == 0 && dateInteger[0] % 100 != 0 ||
+            dateInteger[0] % 400 == 0) {
+            int ans = daysInMonth[dateInteger[1] - 1] + dateInteger[2];
+            return  dateInteger[1] > 2 ? ans + 1 : ans;
         }
         else {
-            runYear = false;
+            return daysInMonth[dateInteger[1] - 1] + dateInteger[2];
         }
-        
-        if (runYear) {
-            ans = daysInMonth[1][dateInteger[1] - 1] + dateInteger[2];
-        }
-        else {
-            ans = daysInMonth[0][dateInteger[1] - 1] + dateInteger[2];
-        }
-       
-       return ans; 
     }
 }
+
 
