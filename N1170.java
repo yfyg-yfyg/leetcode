818a3939c8a9d083fced15eece24f85ad37c76a0
silentstone <3944679@qq.com>
2020-11-11 09:24:24 -0600

    refine

--- N1170.java
+++ N1170.java
@@ -1,6 +1,6 @@
 class Solution {
     
-     static public int calMinFreq(String str) {
+     static private int calMinFreq(String str) {
          int count = 1;
          char minChar = str.charAt(0);
          for(int i = 1; i < str.length(); i ++) {
@@ -16,24 +16,49 @@ class Solution {
          return count;
     }
     
+    static private int calMinFreq1(String str) {
+        int[] freq = new int[26];
+        for (int i = 0; i < str.length(); i ++) {
+            freq[str.charAt(i) - 'a'] ++;
+        }
+
+        int minFreq= 0; 
+        for (int i = 0; i < 26; i ++) {
+            if (freq[i] != 0) {
+                minFreq = freq[i];
+                break;
+            }
+        }
+
+
+        return minFreq;
+    }
+
+    static private int binarySearch(int[] array, int value) {
+        int low = 0, mid = 0, high = array.length - 1;
+        
+        while(low <= high) {
+            mid = (low + high) / 2;
+            if (array[mid] <= value) {
+                low = mid + 1;
+            }
+            else if (array[mid] > value) {
+                high = mid - 1;
+            }
+            else {
+                break;
+            }
+        }
+        
+       return  array.length - low ;
+    }
+    
     public int[] numSmallerByFrequency(String[] queries, String[] words) {
         
-      /*
-        Map<Character, Long> map = 
-                       Arrays.stream(queries)
-                       .map(str -> str.toCharArray().stream()  
-                       .collect(groupingBy(Function.identity(), Collectors.counting()))); 
-        
-        System.out.println(map);
-     */
-        
-      IntStream  queriesFreq = Arrays.stream(queries).mapToInt(Solution::calMinFreq);
-      return queriesFreq.map( query -> {
-                                  return (int) Arrays.stream(words)
-                                              .mapToInt(Solution::calMinFreq)
-                                              .filter(word -> word > query).count();
-                            })
-                            .toArray(); 
+        int[] queriesFreq = Arrays.stream(queries).mapToInt(Solution::calMinFreq).toArray();
+        int[] wordsFreq = Arrays.stream(words).mapToInt(Solution::calMinFreq).sorted().toArray();
+
+        return Arrays.stream(queriesFreq).map( query -> Solution.binarySearch(wordsFreq, query)).toArray();
     }
 }
 
