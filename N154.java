857ae274fc46bd262d8839143e079255511cf8d0
silentstone <3944679@qq.com>
2020-11-13 18:21:20 -0600

    more concise

--- N154.java
+++ N154.java
@@ -4,26 +4,19 @@ class Solution {
 
         while(left < right) {
             int mid = (left + right) / 2;
-            if (nums[left] > nums[left + 1]) {
-                return nums[left + 1];
-            }
-            
-            if (left != 0 && nums[left] < nums[left -1]) {
-                return nums[left];
-            }
-            
-            if (nums[mid] == nums[0]) {
-                left ++;
-            }
-            else if (nums[mid] > nums[0]) {
+            if (nums[mid] > nums[right]) {
                 left = mid + 1;
             }
-            else if (nums[mid] < nums[0]) {
+            else if (nums[mid] < nums[right]) {
                 right = mid;
             }
+            else {
+                right --;
+            }
         }
-
-        return  Math.min(nums[right], nums[0]);
+        
+        return  nums[left];
+            
     }
 }
 
