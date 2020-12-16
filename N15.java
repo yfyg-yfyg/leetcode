cfb86e084bc3275234ef99cfa19887d8b0e305b9
silentstone <3944679@qq.com>
2020-12-16 12:41:05 -0600

    use two pointer

--- N15.java
+++ N15.java
@@ -1,22 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        HashSet<List<Integer>> ans = new HashSet<>();
+        
+        List<List<Integer>> ans = new ArrayList<>(); 
+        
         for (int i = 0; i < nums.length - 2; i ++) {
-            for (int j = i + 1; j < nums.length - 1; j ++) {
-                int loc = Arrays.binarySearch(nums, j + 1, nums.length, -(nums[i] + nums[j]));
-                if (loc >= 0) {
-                    ArrayList<Integer> e = new ArrayList<>(3);
-                    e.add(nums[i]);
-                    e.add(nums[j]);
-                    e.add(nums[loc]);
-                    ans.add(e);
+            if (i == 0 || i > 0 && nums[i] != nums[i - 1]) {
+                int lo = i + 1, hi = nums.length - 1, sum = - nums[i];
+                while(lo < hi) {
+                    if ( sum == nums[lo] + nums[hi]) {
+                        ans.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while (lo < hi && nums[lo] == nums[lo + 1]) lo ++;
+                        while (lo < hi && nums[hi] == nums[hi - 1]) hi --;
+                        lo ++; hi --;
+                    }
+                    else if (nums[lo] + nums[hi] < sum) {
+                        lo ++;
+                    }
+                    else {
+                        hi --;
+                    }
                 }
             }
         }
         
-       return ans.stream().collect(Collectors.toList());
-
+        return ans;
     }
 }
-
